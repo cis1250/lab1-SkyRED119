4abead85bf948b3c3f183bd3bb2217c30ef6eaa9
Amir Kayumov <amir.kayumov7@gmail.com>
2025-09-16 14:47:21 +0000

    Finished lab 1

--- hello.py
+++ hello.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 
-# This is your first Python program!
-# TODO:
-# 1. Keep "Hello, world!" as the first line.
-# 2. Add another print statement with your name.
-# 3. Add another print statement with your favorite hobby.
-
-
-str hobby = "baseball"
-
 print("Hello, world!")
 print("My name is Amir Kayumov!")
-print(f"My favourite hobby is {hobby}")
+
+hobby = "gaming"
+print(f"My favourite hobby is {hobby}!")
