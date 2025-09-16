482d601000d66d5c701cc3a7fec2a1745fdaf957
Amir Kayumov <amir.kayumov7@gmail.com>
2025-09-16 10:40:27 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,8 @@
 # 1. Keep "Hello, world!" as the first line.
 # 2. Add another print statement with your name.
 # 3. Add another print statement with your favorite hobby.
+
+
 str hobby = "baseball"
 
 print("Hello, world!")
